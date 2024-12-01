92eec09e946a24cb36ae2ec2dad36b4968b363ef
Savio Anjos <savioanjos2020@gmail.com>
2024-12-01 08:01:32 -0300

    feat: :sparkles: Added list all jobs controller

---
+++ API/src/http/controllers/jobs/list-all-jobs.ts
@@ -0,0 +1,22 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import { makeListAllJobsUseCase } from "@/use-cases/factories/make-list-all-jobs-use-case";
+import { NoJobsFoundError } from "@/use-cases/errors/no-jobs-found-error";
+
+export async function listAllJobs(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  try {
+    const createJobUseCase = makeListAllJobsUseCase();
+
+    const { jobs } = await createJobUseCase.execute({});
+
+    return reply.status(200).send(jobs);
+  } catch (error) {
+    if (error instanceof NoJobsFoundError) {
+      return reply.status(404).send({ message: error.message });
+    }
+
+    throw error;
+  }
+}
--- API/src/http/controllers/jobs/routes.ts
+++ API/src/http/controllers/jobs/routes.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from "fastify";
 import { createJob } from "./create-job";
 import { verifyJWT } from "../middlewares/fastify-jwt";
+import { listAllJobs } from "./list-all-jobs";
 
 export async function jobsRoutes(app: FastifyInstance) {
   app.post("/jobs", { onRequest: verifyJWT }, createJob);
+  app.get("/jobs", { onRequest: verifyJWT }, listAllJobs);
 }
