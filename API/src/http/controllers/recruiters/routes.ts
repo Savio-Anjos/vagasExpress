45ef59bba7d082b27fa0667e0944248d9c9bbe06
Savio Anjos <savioanjos2020@gmail.com>
2024-11-30 23:03:02 -0300

    feat: :sparkles: Added authenticate recruiter controller

--- API/src/http/controllers/recruiters/routes.ts
+++ API/src/http/controllers/recruiters/routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { createRecruiter } from "./create-recruiter";
+import { authenticateRecruiter } from "./authenticate-recruiter";
 
 export async function recruitersRoutes(app: FastifyInstance) {
   app.post("/recruiters", createRecruiter);
+  app.post("/recruiters/auth", authenticateRecruiter);
 }
