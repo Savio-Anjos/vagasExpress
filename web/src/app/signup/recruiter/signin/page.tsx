3b1bad9d523c779f5b515c301c647e28972f50f8
Savio Anjos <savioanjos2020@gmail.com>
2024-12-01 17:17:19 -0300

    feat: :sparkles: Added signin recruiter page

---
+++ web/src/app/signup/recruiter/signin/page.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import { FormEvent, useState } from "react";
+import styles from "./page.module.css";
+
+export default function SignInRecruiter() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    if (!email || !password) {
+      alert("Por favor, preencha todos os campos.");
+      return;
+    }
+
+    console.log({ email, password });
+  };
+
+  return (
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <h1 className={styles.title}>Login</h1>
+
+        <div className={styles.field}>
+          <label htmlFor="email">E-mail *</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Digite seu e-mail"
+            required
+          />
+        </div>
+
+        <div className={styles.field}>
+          <label htmlFor="password">Senha *</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Digite sua senha"
+            required
+          />
+        </div>
+
+        <button type="submit" className={styles.button}>
+          Entrar
+        </button>
+      </form>
+    </div>
+  );
+}
